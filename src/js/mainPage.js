32040091c0c182559e0f65068d81abde8e4ff8b9
avernikoz <avernikoz@gmail.com>
2018-01-21 15:48:34 +0300

    html is ready

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -96,7 +96,119 @@ let Greeting = React.createClass({
 // );
 
 
-const CONTACTS = [
+// const CONTACTS = [
+//     {
+//         id: 1,
+//         name: 'Darth Vader',
+//         phoneNumber: '+250966666666',
+//         image: 'src/img/darth.gif',
+//         email: 'superdarthvader@gmail.com'
+//     }, {
+//         id: 2,
+//         name: 'Princess Leia',
+//         phoneNumber: '+250966344466',
+//         image: 'src/img/leia.gif',
+//         email: 'PrincessLeia@gmail.com'
+//
+//     }, {
+//         id: 3,
+//         name: 'Luke Skywalker',
+//         phoneNumber: '+250976654433',
+//         image: 'src/img/luke.gif',
+//         email: 'LukeSkywalker@gmail.com'
+//     }, {
+//         id: 4,
+//         name: 'Chewbacca',
+//         phoneNumber: '+250456784935',
+//         image: 'src/img/chewbacca.gif',
+//         email: 'LukeSkywalker@gmail.com'
+//     }
+// ];
+
+
+// let ContactsPanel = React.createClass({
+//     getInitialState: function () {
+//         return {
+//             displayedContacts: CONTACTS,
+//             personWhomWeSayHello: 'stranger'
+//                 };
+//     },
+//     chageGreetingText: function (event) {
+//         this.setState({personWhomWeSayHello : event.target.value});
+//     },
+//     searchFunction: function (event) {
+//         let searchQuery = event.target.value.toLowerCase();
+//         let displayedContacts = CONTACTS.filter((element) => {
+//             let contactsName = element.name.toLowerCase();
+//             let contactsPhone = element.phoneNumber;
+//
+//             return (contactsName.indexOf(searchQuery) !== -1 || contactsPhone.indexOf(searchQuery) !== -1);
+//         });
+//
+//         this.setState({displayedContacts: displayedContacts});
+//
+//         console.log(displayedContacts);
+//     },
+//     render: function () {
+//         return (
+//             <div className="contacts">
+//                 <input type="text" className="search-field" onChange={this.searchFunction}/>
+//                 <div className="contacts-list">
+//                     {
+//                         this.state.displayedContacts.map((element) => {
+//                             return <Contact key={element.id} name={element.name} phoneNumber={element.phoneNumber}
+//                                             image={element.image} email={element.email}/>
+//                         })
+//                     }
+//                 </div>
+//                 <input type="text" className="search-field" onChange={this.chageGreetingText}/>
+//                 <p>Hello,{' '+this.state.personWhomWeSayHello}</p>
+//             </div>
+//         )
+//     }
+// });
+//
+//
+// let Contact = React.createClass({
+//     getInitialState: function () {
+//         return {expanded: false}
+//     },
+//     contactExpand: function (event) {
+//         this.setState({expanded: (this.state.expanded ? false : true)})
+//     },
+//     render: function () {
+//
+//         if (this.state.expanded) {
+//             return (
+//                 <div className="contact expanded" onClick={this.contactExpand}>
+//                     <img className="contact-image" src={this.props.image} height="50px" width="50px"/>
+//                     <div>
+//                         <div className="contact-name">{this.props.name}</div>
+//                         <div className="contact-number">{this.props.phoneNumber}</div>
+//                         <div className="contact-number">{this.props.email}</div>
+//                     </div>
+//                 </div>
+//             )
+//         }
+//         else {
+//             return (
+//                 <div className="contact" onClick={this.contactExpand}>
+//                     <img className="contact-image" src={this.props.image} height="50px" width="50px"/>
+//                     <div className="contact-name">{this.props.name}</div>
+//                     <div className="contact-number">{this.props.phoneNumber}</div>
+//                 </div>
+//             )
+//         }
+//     }
+// });
+//
+//
+// ReactDOM.render(
+//     <ContactsPanel/>,
+//     document.getElementById('root')
+// );
+
+const notes = [
     {
         id: 1,
         name: 'Darth Vader',
@@ -125,87 +237,51 @@ const CONTACTS = [
     }
 ];
 
-{/*<Contact name={CONTACTS[1].name} phoneNumber={CONTACTS[1].phoneNumber} image={CONTACTS[1].image}/>*/
-}
 
-let ContactsPanel = React.createClass({
-    getInitialState: function () {
-        return {
-            displayedContacts: CONTACTS,
-            personWhomWeSayHello: 'stranger'
-                };
-    },
-    chageGreetingText: function (event) {
-        this.setState({personWhomWeSayHello : event.target.value});
-    },
-    searchFunction: function (event) {
-        let searchQuery = event.target.value.toLowerCase();
-        let displayedContacts = CONTACTS.filter((element) => {
-            let contactsName = element.name.toLowerCase();
-            let contactsPhone = element.phoneNumber;
-
-            return (contactsName.indexOf(searchQuery) !== -1 || contactsPhone.indexOf(searchQuery) !== -1);
-        });
-
-        this.setState({displayedContacts: displayedContacts});
-
-        console.log(displayedContacts);
-    },
+let Note = React.createClass({
     render: function () {
         return (
-            <div className="contacts">
-                <input type="text" className="search-field" onChange={this.searchFunction}/>
-                <div className="contacts-list">
-                    {
-                        this.state.displayedContacts.map((element) => {
-                            return <Contact key={element.id} name={element.name} phoneNumber={element.phoneNumber}
-                                            image={element.image} email={element.email}/>
-                        })
-                    }
-                </div>
-                <input type="text" className="search-field" onChange={this.chageGreetingText}/>
-                <p>Hello,{' '+this.state.personWhomWeSayHello}</p>
+            <div className="note">Note</div>
+        )
+    }
+});
+
+let NotesEditor = React.createClass({
+    render: function () {
+        return (
+            <div className="notes-editor">
+                <textarea name="editor" cols="30" rows="10" placeholder="Текст заметки"/>
+                <input type="button" value="Добавить"/>
+            </div>
+        )
+    }
+});
+
+let NotesContainer = React.createClass({
+    render: function () {
+        return (
+            <div className="notes-container">
+                <Note/>
+                <Note/>
+                <Note/>
+            </div>
+        )
+    }
+});
+
+let NotesApp = React.createClass({
+    render: function () {
+        return (
+            <div className="notes-app">
+                <NotesEditor/>
+                <NotesContainer/>
             </div>
         )
     }
 });
 
 
-let Contact = React.createClass({
-    getInitialState: function () {
-        return {expanded: false}
-    },
-    contactExpand: function (event) {
-        this.setState({expanded: (this.state.expanded ? false : true)})
-    },
-    render: function () {
-
-        if (this.state.expanded) {
-            return (
-                <div className="contact expanded" onClick={this.contactExpand}>
-                    <img className="contact-image" src={this.props.image} height="50px" width="50px"/>
-                    <div>
-                        <div className="contact-name">{this.props.name}</div>
-                        <div className="contact-number">{this.props.phoneNumber}</div>
-                        <div className="contact-number">{this.props.email}</div>
-                    </div>
-                </div>
-            )
-        }
-        else {
-            return (
-                <div className="contact" onClick={this.contactExpand}>
-                    <img className="contact-image" src={this.props.image} height="50px" width="50px"/>
-                    <div className="contact-name">{this.props.name}</div>
-                    <div className="contact-number">{this.props.phoneNumber}</div>
-                </div>
-            )
-        }
-    }
-});
-
-
 ReactDOM.render(
-    <ContactsPanel/>,
+    <NotesApp />,
     document.getElementById('root')
 );
