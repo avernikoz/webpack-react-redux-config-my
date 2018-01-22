71f78872d2ed60e41670faa0d0ee7b14cf683114
avernikoz <avernikoz@gmail.com>
2018-01-22 22:35:27 +0300

    note list is working well

--- src/js/mainPage.js
+++ src/js/mainPage.js
@@ -2,6 +2,7 @@
 // Вы пишите так let RandomMessage = React.createClass или extends.React ???
 // Какой скелет для реакта юзаете?
 // state атоматом передаётся в пропсы дочерних элементов, или нет?
+// 12:04
 
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -10,61 +11,121 @@ import createReactClass from 'create-react-class';
 
 React.createClass = createReactClass;
 
+
 const notes = [
     {
         id: 1,
-        name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cupiditate, eos! Adipisci autem harum quos!',
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis cupiditate, eos! Adipisci autem harum quos!',
         color: '#3444FF'
     }, {
         id: 2,
-        name: 'veritatis? Eos illum laudantium recusandae repellendus similique. Consequatur dolorem nesciunt numquam possimus soluta! Provident.',
+        text: 'veritatis? Eos illum laudantium recusandae repellendus similique. Consequatur dolorem nesciunt numquam possimus soluta! Provident.',
         color: '#FF113A'
     }, {
         id: 3,
-        name: 'Amet aspernatur autem facilis laborum molestias praesentium reiciendis rerum elit. Excepturi, voluptatum.',
+        text: 'Amet aspernatur autem facilis laborum molestias praesentium reiciendis rerum elit. Excepturi, voluptatum.',
         color: '#19FF2C'
     }, {
         id: 4,
-        name: 'dolorem dolores, ea eligendi facilis fuga harum illum in ipsum itaque.',
+        text: 'dolorem dolores, ea eligendi facilis fuga harum illum in ipsum itaque.',
         color: '#FFD700'
     }, {
         id: 5,
-        name: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi.,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi.',
         color: '#E88AFF'
     }, {
         id: 6,
-        name: 'optio perferendis quidem quisquam recusandae repellendus suscipit veniam',
+        text: 'optio perferendis quidem quisquam recusandae repellendus suscipit veniam',
         color: '#9AFF8F'
     }, {
         id: 7,
-        name: 'ducimus eos eum expedita explicabo incidunt inventore iste iure labore minus nostrum nulla,',
+        text: 'ducimus eos eum expedita explicabo incidunt inventore iste iure labore minus nostrum nulla,',
         color: '#FF3BBA'
     }, {
         id: 8,
-        name: 'consequuntur culpa cum cupiditate deserunt distinctio,',
+        text: 'consequuntur culpa cum cupiditate deserunt distinctio,',
         color: '#B91BFF'
     }, {
         id: 9,
-        name: 'unde veritatis vitae voluptates voluptatibus! ',
+        text: 'unde veritatis vitae voluptates voluptatibus! ',
         color: '#00FFF9'
     }
 ];
 
+function getRandomColor() {
+    let letters = '789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 9)];
+    }
+    return color;
+}
 
-let Note = React.createClass({
+
+
+
+
+
+
+
+
+
+
+
+let NotesEditor = React.createClass({
+    getInitialState: function () {
+        return {textInput: ''}
+    },
+    inputTextChanged: function (event) {
+        this.setState({textInput: event.target.value});
+        console.log(event.target.value);
+    },
+    addNote: function () {
+        let newNote = {
+            id: Date.now(),
+            text: this.state.textInput,
+            color: getRandomColor()
+        };
+
+        this.props.onNotesAdd(newNote);
+        this.setState({textInput: ''});
+    },
     render: function () {
         return (
-            <div className="note">Note</div>
+            <div className="notes-editor">
+                <textarea className="textarea" name="editor" cols="30" rows="10" placeholder="Текст заметки"
+                          value={this.state.textInput} onChange={this.inputTextChanged}/>
+                <input className="add-button" type="button" value="Добавить" onClick={this.addNote}/>
+            </div>
         )
     }
 });
 
-let NotesEditor = React.createClass({
+let DeleteButton = React.createClass({
+    hello: function (event) {
+        console.log('hello', event.target.value);
+    },
     render: function () {
         return (
-            <div className="notes-editor">
-                <textarea className="textarea" name="editor" cols="30" rows="10" placeholder="Текст заметки"/>
-                <input className="add-button" type="button" value="Добавить"/>
+            <div onClick={this.props.onDeleteNote} className="delete-button">x</div>
+        )
+    }
+});
+
+let Note = React.createClass({
+    deleteNote: function () {
+        this.props.deleteHandler(this.props.id);
+    },
+    render: function () {
+        let notesStyle = {
+            backgroundColor: this.props.color,
+            position: 'relative'
+        };
+
+        return (
+            <div style={notesStyle} className="note">
+                <DeleteButton onDeleteNote={this.deleteNote} />
+                {this.props.children}
             </div>
         )
     }
@@ -74,28 +135,60 @@ let NotesContainer = React.createClass({
     render: function () {
         return (
             <div className="notes-container">
-                <Note/>
-                <Note/>
-                <Note/>
+                {
+                    this.props.notes.map((element) => {
+                        return <Note deleteHandler={this.props.deleteFunction} id={element.id} key={element.id} color={element.color}>{element.text}</Note>
+                    })
+                }
             </div>
         )
     }
 });
 
 let NotesApp = React.createClass({
+    getInitialState: function () {
+        return ({notes: []})
+    },
+    componentDidMount: function () {
+        let notesFromLocalStorage = JSON.parse(localStorage.getItem('notes'));
+        if (notesFromLocalStorage){
+            this.setState({notes: notesFromLocalStorage});
+        }
+    },
+    componentDidUpdate: function () {
+        this.updateLocalStorage()
+    },
+    addNoteInContainer: function (newNote) {
+        let oldNotes = this.state.notes;
+        oldNotes.push(newNote);
+
+        this.setState({notes: oldNotes})
+    },
+    deleteNoteInContainer: function (noteIdToDelete) {
+
+        let notesAfterDeleting = this.state.notes.filter((element) => {
+            return element.id !== noteIdToDelete;
+        });
+
+        this.setState({notes: notesAfterDeleting});
+    },
     render: function () {
         return (
             <div className="notes-app">
-                <NotesEditor/>
-                <NotesContainer/>
+                <NotesEditor onNotesAdd={this.addNoteInContainer}/>
+                <NotesContainer deleteFunction={this.deleteNoteInContainer} notes={this.state.notes}/>
             </div>
         )
+    },
+    updateLocalStorage: function () {
+        let notes = JSON.stringify(this.state.notes);
+        localStorage.setItem('notes', notes);
     }
 });
 
 
 ReactDOM.render(
-    <NotesApp />,
+    <NotesApp/>,
     document.getElementById('root')
 );
 
