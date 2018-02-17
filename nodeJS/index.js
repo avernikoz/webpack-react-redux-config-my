b5b4a17553138599527f4c55d7971daf19d204bb
avernikoz <avernikoz@gmail.com>
2018-02-17 21:40:01 +0300

    minimal refactor

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -2,17 +2,16 @@
 // 2. Почему если использовать // logWriteStream.end(); то происходят ошибки
 // 3. Можно ли как-то реализовать retry(3) по-другому?
 // 4. Как отследить, что файл, в который открыт поток, был удалён?
+// 5. Два ретурна - это бэд прэктис?
+// 6. Promise.all даже если были reject
 
-// Google key
-// AIzaSyBJQ8HoYiJAR3PKDxYkl9MGYD0LcJaTgG4
+const config = require('./config');
 
 const fs = require('fs');
 const mkdirp = require('mkdirp').sync;
 const getDirName = require('path').dirname;
 
 const fetch = require('node-fetch');
-const googleApiKey = 'AIzaSyBJQ8HoYiJAR3PKDxYkl9MGYD0LcJaTgG4';
-
 const commandArgs = process.argv.slice(2);
 
 if (commandArgs < 2) {
@@ -25,7 +24,7 @@ const outputFilePath = commandArgs[1];
 
 
 // logger
-const logWriteStream = fs.createWriteStream('log.txt', {'flags': 'a', 'encoding': 'utf8'}).on('error', (err) => {
+const logWriteStream = fs.createWriteStream(config.log.name, {'flags': 'a', 'encoding': config.log.encoding}).on('error', (err) => {
     console.error(`ERROR: + ${err}`);
 });
 
@@ -35,27 +34,28 @@ if (!fs.existsSync(getDirName(outputFilePath))){
         if (err) throw Error(`${err}`);
     }));
 }
-const outputWriteStream = fs.createWriteStream(outputFilePath, {'flags': 'a', 'encoding': 'utf8'}).on('error', (err) => {
+const outputWriteStream = fs.createWriteStream(outputFilePath, {'flags': 'a', 'encoding': config.output.encoding}).on('error', (err) => {
     console.error(`ERROR: + ${err}`);
 });
 
 
-fs.readFile(inputFilePath, 'utf8', (err, data) => {
+fs.readFile(inputFilePath, config.input.encoding, (err, data) => {
 
     if (!err) {
         const regexopt = new RegExp(/\n|\r/gi);
         let urlArray = data.split(regexopt);
 
         urlArray.forEach((currentUrl, currentUrlIndex) => {
-            let googlePageSpeedUrl = `https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=${currentUrl}&strategy=desktop&key=${googleApiKey}`;
+            let googlePageSpeedUrl = `${config.api.url}?url=${currentUrl}&strategy=${config.api.strategy}&key=${config.api.key}`;
 
             console.log(googlePageSpeedUrl);
 
 
             setTimeout(() => {
-                fetch(googlePageSpeedUrl, {timeout: 15000})
+                fetch(googlePageSpeedUrl, {timeout: config.fetch.timeout})
                     .then(res => res.json())
                     .then(res => {
+                            // Refactor this or NOT?
                             if (res.hasOwnProperty('error')) {
                                 return Promise.reject(new Error(`${res.error.message}`));
                             }
@@ -68,34 +68,39 @@ fs.readFile(inputFilePath, 'utf8', (err, data) => {
                         outputWriteStream.write(`${JSON.stringify(res, null, '\t')} \n`);
                     })
                     .then(() => {
-                        logWriteStream.write(`[ ${new Date().toString()} ] Url processed well!: ${currentUrl}\n`);
+                        logWriteStream.write(`[ ${new Date().toString()} ] ${config.fetch.completedText}: ${currentUrl}\n`);
                     })
                     .catch(err => {
-                        // console.error(err);
+                        //Refactor this
                         if (err.code === 'ENOTFOUND') {
-                            console.log(`Can't connect to the current API, maybe internet connection was lost.`);
-                            logWriteStream.write(`[ ${new Date().toString()} ] Url NOT PROCESSED!: ${currentUrl}    ERROR: ENOTFOUND: Can't connect to the current API, maybe internet connection was lost  \n`);
+                            console.log(config.error.ENOTFOUND);
+                            logWriteStream.write(`[ ${new Date().toString()} ] ${config.fetch.inCompletedText}: ${currentUrl}    ERROR: ${config.error.ENOTFOUND}  \n`);
                         }
                         else if (err.type === 'request-timeout') {
-                            console.log(`Timeout error. Slow internet connection OR API server have fall down.`);
-                            logWriteStream.write(`[ ${new Date().toString()} ] Url NOT PROCESSED!: ${currentUrl}    ERROR: request-timeout: Timeout error. Slow internet connection OR API server have fallen down.  \n`);
+                            console.log(config.error.TIMEOUT);
+                            logWriteStream.write(`[ ${new Date().toString()} ] ${config.fetch.inCompletedText}: ${currentUrl}    ERROR: ${config.error.TIMEOUT}  \n`);
                         }
                         else {
                             console.log(err.message);
-                            logWriteStream.write(`[ ${new Date().toString()} ] Url NOT PROCESSED!: ${currentUrl}    ERROR: ${err.message}  \n`);
+                            logWriteStream.write(`[ ${new Date().toString()} ] ${config.fetch.inCompletedText}: ${currentUrl}    ERROR: ${err.message}  \n`);
                         }
                     });
-            }, currentUrlIndex * 3500);
+            }, currentUrlIndex * config.fetch.intervalBetweenEachRequest);
 
 
         });
 
 
     }
-    // Need to refactor to ENOENT
+    else if (err.code === 'ENOENT') {
+        logWriteStream.write(`[ ${new Date().toString()} ] ${config.error.ENOENT}: '${inputFilePath}'\n`, config.log.encoding, () => {
+            console.error(config.error.ENOENT);
+        });
+        logWriteStream.end();
+    }
     else {
-        logWriteStream.write(`[ ${new Date().toString()} ] File not found at current path: '${inputFilePath}'\n`, 'utf-8', () => {
-            console.error('Input file with urls not found!');
+        logWriteStream.write(`[ ${new Date().toString()} ] ${err.message} \n`, config.log.encoding, () => {
+            console.error(err.message);
         });
         logWriteStream.end();
     }
