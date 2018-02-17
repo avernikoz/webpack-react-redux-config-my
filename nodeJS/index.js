d6c559a39e2a3ff3ff58d282379492185dd389c2
avernikoz <avernikoz@gmail.com>
2018-02-17 13:49:42 +0300

    loggin our script added

--- nodeJS/index.js
+++ nodeJS/index.js
@@ -1,45 +1,68 @@
+//1. Можео ли вызвать функцию без аргумента по середине? func(a,b[empty],c)
+
 // Google key
 // AIzaSyBJQ8HoYiJAR3PKDxYkl9MGYD0LcJaTgG4
-
 
 
 const fs = require('fs');
 const fetch = require('node-fetch');
 const googleApiKey = 'AIzaSyBJQ8HoYiJAR3PKDxYkl9MGYD0LcJaTgG4';
 
+// logger
+let logWriteStream = fs.createWriteStream('log.txt', { 'flags': 'a', 'encoding': 'utf8'});
+
+// logWriteStream.on('finish', (e) => {
+//     console.error('Input file with urls not found!');
+// });
+logWriteStream.on('error', () => {
+    console.error('Error in writing to log file!');
+});
+// logger
+
 
 let commandArgs = process.argv.slice(2);
 
 
-if (commandArgs < 2){
+if (commandArgs < 2) {
     throw new Error('Not all arguments are passed to the script!');
     // console.error('Not all arguments are passed to the script!');
 }
+// console.log(process.argv);
 
 let inputFilePath = commandArgs[0];
 let outputFilePath = commandArgs[1];
 
-// console.log(process.argv);
 
 
-let inputFile = fs.readFile(inputFilePath,'utf8', (err, data) => {
+
+
+let inputFile = fs.readFile(inputFilePath, 'utf8', (err, data) => {
 
     if (!err) {
         let regexopt = new RegExp(/\n|\r/gi);
         let urlArray = data.split(regexopt);
 
-        // console.log(urlArray[0]);
+        let googlePageSpeedUrl = `https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=${urlArray[12]}&strategy=desktop&key=${googleApiKey}`;
 
-        let googlePageSpeedUrl = `https://www.googleapis.com/pagespeedonline/v4/runPagespeed?url=${urlArray[3]}&strategy=desktop&key=${googleApiKey}`;
-
-        fetch(googlePageSpeedUrl)
+        fetch(googlePageSpeedUrl, {timeout: 15000})
             .then(res => res.text())
-            .then(res => fs.writeFile(outputFilePath,res,'utf8'))
-            .then(body => console.log(body));
+            .then(res => {
+                fs.writeFile(outputFilePath, res, 'utf8');
+            })
+            // .then(body => console.log(body))
+            .then(() => {
+                logWriteStream.write(`[ ${new Date().toString()} ] Url processed well!: {current url}\n`);
+                logWriteStream.end();
+            })
+            .catch(err => {
+                console.error(err);
+            });
     }
     else {
-        console.error('File not found!');
-
+        logWriteStream.write(`[ ${new Date().toString()} ] File not found at current path: '${inputFilePath}'\n`,'utf-8',()=>{
+            console.error('Input file with urls not found!');
+        });
+        logWriteStream.end();
     }
 
 });
