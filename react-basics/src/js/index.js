5811af4c9e94888693b71831701440a617762328
avernikoz <avernikoz@gmail.com>
2018-03-28 17:14:52 +0300

    fixed names

--- react-basics/src/js/index.js
+++ react-basics/src/js/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ToDoListApp from './ToDoListApp';
+import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
 
 
 ReactDOM.render(
     <Router>
-        <ToDoListApp/>
+        <App/>
     </Router>,
     document.getElementById('root')
 );
