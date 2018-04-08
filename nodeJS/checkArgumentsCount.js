b0891b18d767affbe70d31b48951f07fad5741c9
avernikoz <avernikoz@gmail.com>
2018-04-08 20:17:29 +0300

    delete nodeJS task

--- nodeJS/checkArgumentsCount.js
+++
@@ -1,8 +0,0 @@
-module.exports = (commandArgsArray) => {
-    if (commandArgsArray.length < 2) {
-        throw new Error('Not all arguments are passed to the script!');
-    }
-    else {
-        return commandArgsArray;
-    }
-};
