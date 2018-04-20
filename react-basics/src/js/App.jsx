ab507b0bfa645e9a09611438c08d2bee78e31f47
avernikoz <avernikoz@gmail.com>
2018-04-20 13:07:08 +0300

    final edit, delete all trash from working directory

--- react-basics/src/js/App.jsx
+++ react-basics/src/js/App.jsx
@@ -1,20 +1,4 @@
-// 1. Один из вопросов — всё-таки, как лучше заюзать коннект — использовать его во всех компонентах,
-// или же иногда спускать пропсы сверху?
-// https://github.com/reactjs/redux/issues/419
-// 2. Где лучше прописывать propTypes — для statless компонентов родительских, или для дочерних ///
-// 3. Как избежать ререндера родительского компонента при измененни пропсов, которые он использует?
-
 import React from 'react';
-
-import { Route, Switch, withRouter } from 'react-router-dom';
-
-import NoMatch from './components/NoMatch';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import ContactsList from './containers/ContactsList';
-import SearchFilter from './containers/SearchFilter';
-import Modal from './containers/Modal';
-import AddContactButton from './containers/AddContactButton';
 
 // import '../scss/style.scss';
 
--- react-basics/src/js/index.jsx
+++ react-basics/src/js/index.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 // import { devToolsEnhancer } from 'redux-devtools-extension';
 
-import { rootReducer, initialState } from './reducers/reducers';
+// import { rootReducer, initialState } from './reducers/reducers';
 import App from './App';
 
 
 // const reducers = createStore(rootReducer, initialState, devToolsEnhancer());
-const store = createStore(rootReducer, initialState);
+// const store = createStore(rootReducer, initialState);
 
 
 ReactDOM.render(
-    <Provider store={store}>
-            <App />
-    </Provider>,
+        <App />,
     document.getElementById('root')
 );
+
+
+// ReactDOM.render(
+//     <Provider store={store}>
+//             <App />
+//     </Provider>,
+//     document.getElementById('root')
+// );
